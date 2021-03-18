58bdf307dcc47aa86278fd22d016708f31ea9898
LOCAL14 <kapaxiayu@hotmail.com>
2021-03-18 22:50:02 +0800

    first commit

---
+++ ColorWeather.js
@@ -0,0 +1,12 @@
+var obj = JSON.parse(body);
+obj.result["xy_vip_expire"] = 4096483190;
+obj.result["is_vip"] = true;
+obj.result["vip_expired_at"] = 4096483190;
+obj.result["svip_expired_at"] = 4096483190;
+obj.result["xy_svip_expire"] = 4096483190;
+obj.result["is_xy_vip"] = true;
+obj.result["vip_type"] = "svip";
+body = JSON.stringify(obj);
+$done({
+  body,
+});
