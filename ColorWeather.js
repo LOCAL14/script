cd79d69922c7693ff8cce57e273bc87116ccc195
LOCAL14 <kapaxiayu@hotmail.com>
2021-03-19 03:59:59 +0000

    4

--- ColorWeather.js
+++ ColorWeather.js
@@ -6,7 +6,15 @@ obj.result["svip_expired_at"] = 4096483190;
 obj.result["xy_svip_expire"] = 4096483190;
 obj.result["is_xy_vip"] = true;
 obj.result["vip_type"] = "svip";
-console.log(obj);
+obj.result.wt = {
+  vip: {
+    is_auto_renewal: false,
+    enabled: true,
+    auto_renewal_type: "",
+    expired_at: 4096483190,
+  },
+};
+
 console.log(JSON.stringify(obj));
 
 $done({ body: JSON.stringify(obj) });
