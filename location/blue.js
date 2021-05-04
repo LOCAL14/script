5f78c2f954ecce15923ad6e1bf16974392038646
夏震 <xiazhen@xzdemacbook-pro.local>
2021-05-04 19:57:22 +0000

    blue

--- location/blue.js
+++ location/blue.js
@@ -6,7 +6,7 @@ try {
 
 	if (path && path.search(/longitude/) > 0) {
 		
-		console.log("特殊定位修改：捕获成功");
+		console.log("特殊定位修改：捕获成功--"+$request.url);
 		
 		if(path.search(/\&longitude=\d+.\d+\&/) > 0){
 			path = path.replace(/\&longitude=\d+.\d+\&/, '&longitude=120.69094848632812&')
@@ -22,6 +22,28 @@ try {
 			path = path.replace(/\&latitude=\d+.\d+/, '&latitude=30.526451110839844')
 		}else{
 			console.log("特殊定位修改：latitude修改失败");
+		}
+		
+		console.log("特殊定位修改：成功--" + path)
+		
+	}else if(path && path.search(/lat/) > 0 && path.search(/lot/) > 0){
+	
+		console.log("特殊定位修改：捕获成功--city_code");
+		
+		if(path.search(/\&lot=\d+.\d+\&/) > 0){
+			path = path.replace(/\&lot=\d+.\d+\&/, '&lot=120.69094848632812&')
+		}else if(path.search(/\&lot=\d+.\d+/) > 0){
+			path = path.replace(/\&lot=\d+.\d+/, '&lot=120.69094848632812')
+		}else{
+			console.log("特殊定位修改：lot修改失败");
+		}
+		
+		if(path.search(/\&lat=\d+.\d+\&/) > 0){
+			path = path.replace(/\&lat=\d+.\d+\&/, '&lat=30.526451110839844&')
+		}else if(path.search(/\&lat=\d+.\d+/) > 0){
+			path = path.replace(/\&lat=\d+.\d+/, '&lat=30.526451110839844')
+		}else{
+			console.log("特殊定位修改：lat修改失败");
 		}
 		
 		console.log("特殊定位修改：成功--" + path)
