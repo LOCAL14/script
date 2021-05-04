0d121fd5e5bf7a14daa7959749abb2689b940473
夏震 <xiazhen@xzdemacbook-pro.local>
2021-05-04 19:32:45 +0000

    blue

--- location/blue.js
+++ location/blue.js
@@ -1,7 +1,6 @@
 //特殊定位修改 argo.blue.cn
 
 path = $request.path
-url = $request.url
 
 try {
 
@@ -9,18 +8,18 @@ try {
 		
 		console.log("特殊定位修改：捕获成功");
 		
-		if(path.search(/\&longitude=.+\&/) > 0){
-			path = path.replace(/\&longitude=.+\&/, '&longitude=120.69094848632812&')
-		}else if(path.search(/\&longitude=.+/) > 0){
-			path = path.replace(/\&longitude=.+/, '&longitude=120.69094848632812')
+		if(path.search(/\&longitude=\d+.\d+\&/) > 0){
+			path = path.replace(/\&longitude=\d+.\d+\&/, '&longitude=120.69094848632812&')
+		}else if(path.search(/\&longitude=\d+.\d+\&/) > 0){
+			path = path.replace(/\&longitude=\d+.\d+/, '&longitude=120.69094848632812')
 		}else{
 			console.warn("特殊定位修改：longitude修改失败");
 		}
 		
-		if(path.search(/\&latitude=.+\&/) > 0){
-			path = path.replace(/\&latitude=.+\&/, '&latitude=30.526451110839844&')
+		if(path.search(/\&latitude=\d+.\d\&/) > 0){
+			path = path.replace(/\&latitude=\d+.\d+\&/, '&latitude=30.526451110839844&')
 		}else if(path.search(/\&latitude=.+/) > 0){
-			path = path.replace(/\&latitude=.+/, '&latitude=30.526451110839844')
+			path = path.replace(/\&latitude=\d+.\d+/, '&latitude=30.526451110839844')
 		}else{
 			console.warn("特殊定位修改：latitude修改失败");
 		}
@@ -28,32 +27,10 @@ try {
 		console.log("特殊定位修改：成功--" + path)
 		path = 111
 	}
-	
-	if (url && url.search(/longitude/) > 0) {
-		
-		console.log("特殊定位修改：捕获成功");
-		
-		if(url.search(/\&longitude=.+\&/) > 0){
-			url = url.replace(/\&longitude=.+\&/, '&longitude=120.69094848632812&')
-		}else if(url.search(/\&longitude=.+/) > 0){
-			url = url.replace(/\&longitude=.+/, '&longitude=120.69094848632812')
-		}else{
-			console.warn("特殊定位修改：longitude修改失败");
-		}
-		
-		if(url.search(/\&latitude=.+\&/) > 0){
-			url = url.replace(/\&latitude=.+\&/, '&latitude=30.526451110839844&')
-		}else if(url.search(/\&latitude=.+/) > 0){
-			url = url.replace(/\&latitude=.+/, '&latitude=30.526451110839844')
-		}else{
-			console.warn("特殊定位修改：latitude修改失败");
-		}
-		
-		console.log("特殊定位修改：成功--" + url)
-	}
+
 	
 } catch (e) {
   console.warn("特殊定位修改：" + e);
 } finally {
-  $done({url : url, path : path})
+  $done({path : path})
 }
