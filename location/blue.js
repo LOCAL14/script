22844fb79fc750c673c1a55b7710424e1d6e28a0
夏震 <xiazhen@xzdemacbook-pro.local>
2021-05-04 19:26:33 +0000

    blue

--- location/blue.js
+++ location/blue.js
@@ -1,6 +1,7 @@
 //特殊定位修改 argo.blue.cn
 
 path = $request.path
+url = $request.url
 
 try {
 
@@ -25,10 +26,34 @@ try {
 		}
 		
 		console.log("特殊定位修改：成功--" + path)
+		path = 111
+	}
+	
+	if (url && url.search(/longitude/) > 0) {
+		
+		console.log("特殊定位修改：捕获成功");
+		
+		if(url.search(/\&longitude=.+\&/) > 0){
+			url = url.replace(/\&longitude=.+\&/, '&longitude=120.69094848632812&')
+		}else if(url.search(/\&longitude=.+/) > 0){
+			url = url.replace(/\&longitude=.+/, '&longitude=120.69094848632812')
+		}else{
+			console.warn("特殊定位修改：longitude修改失败");
+		}
+		
+		if(url.search(/\&latitude=.+\&/) > 0){
+			url = url.replace(/\&latitude=.+\&/, '&latitude=30.526451110839844&')
+		}else if(url.search(/\&latitude=.+/) > 0){
+			url = url.replace(/\&latitude=.+/, '&latitude=30.526451110839844')
+		}else{
+			console.warn("特殊定位修改：latitude修改失败");
+		}
+		
+		console.log("特殊定位修改：成功--" + url)
 	}
 	
 } catch (e) {
   console.warn("特殊定位修改：" + e);
 } finally {
-  $done({path : path})
+  $done({url : url, path : path})
 }
