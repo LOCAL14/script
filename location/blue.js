e76b8f6e2dfd69d652ba1b3d6a766820d1e1ebd2
夏震 <xiazhen@xzdemacbook-pro.local>
2021-05-04 19:16:49 +0000

    blue debug

--- location/blue.js
+++ location/blue.js
@@ -4,52 +4,54 @@ path = $request.path
 url = $request.url
 
 try {
-	if (path && path.search(/longitude/) > 0) {
-		
-		console.log("特殊定位修改：捕获成功");
-		
-		if(path.search(/\&longitude=.+\&/) > 0){
-			path = path.replace(/\&longitude=.+\&/, '&longitude=120.69094848632812&')
-		}else if(path.search(/\&longitude=.+/) > 0){
-			path = path.replace(/\&longitude=.+/, '&longitude=120.69094848632812')
-		}else{
-			console.warn("特殊定位修改：longitude修改失败");
-		}
-		
-		if(path.search(/\&latitude=.+\&/) > 0){
-			path = path.replace(/\&latitude=.+\&/, '&latitude=30.526451110839844&')
-		}else if(path.search(/\&latitude=.+/) > 0){
-			path = path.replace(/\&latitude=.+/, '&latitude=30.526451110839844')
-		}else{
-			console.warn("特殊定位修改：latitude修改失败");
-		}
-		
-		console.log("特殊定位修改：成功--" + path)
-		path = 111
-	}
-	
-	if (url && url.search(/longitude/) > 0) {
-		
-		console.log("特殊定位修改：捕获成功");
-		
-		if(url.search(/\&longitude=.+\&/) > 0){
-			url = url.replace(/\&longitude=.+\&/, '&longitude=120.69094848632812&')
-		}else if(url.search(/\&longitude=.+/) > 0){
-			url = url.replace(/\&longitude=.+/, '&longitude=120.69094848632812')
-		}else{
-			console.warn("特殊定位修改：longitude修改失败");
-		}
-		
-		if(url.search(/\&latitude=.+\&/) > 0){
-			url = url.replace(/\&latitude=.+\&/, '&latitude=30.526451110839844&')
-		}else if(url.search(/\&latitude=.+/) > 0){
-			url = url.replace(/\&latitude=.+/, '&latitude=30.526451110839844')
-		}else{
-			console.warn("特殊定位修改：latitude修改失败");
-		}
-		
-		console.log("特殊定位修改：成功--" + url)
-	}
+	path = 111
+	url = 222
+	// if (path && path.search(/longitude/) > 0) {
+	// 	
+	// 	console.log("特殊定位修改：捕获成功");
+	// 	
+	// 	if(path.search(/\&longitude=.+\&/) > 0){
+	// 		path = path.replace(/\&longitude=.+\&/, '&longitude=120.69094848632812&')
+	// 	}else if(path.search(/\&longitude=.+/) > 0){
+	// 		path = path.replace(/\&longitude=.+/, '&longitude=120.69094848632812')
+	// 	}else{
+	// 		console.warn("特殊定位修改：longitude修改失败");
+	// 	}
+	// 	
+	// 	if(path.search(/\&latitude=.+\&/) > 0){
+	// 		path = path.replace(/\&latitude=.+\&/, '&latitude=30.526451110839844&')
+	// 	}else if(path.search(/\&latitude=.+/) > 0){
+	// 		path = path.replace(/\&latitude=.+/, '&latitude=30.526451110839844')
+	// 	}else{
+	// 		console.warn("特殊定位修改：latitude修改失败");
+	// 	}
+	// 	
+	// 	console.log("特殊定位修改：成功--" + path)
+	// 	path = 111
+	// }
+	// 
+	// if (url && url.search(/longitude/) > 0) {
+	// 	
+	// 	console.log("特殊定位修改：捕获成功");
+	// 	
+	// 	if(url.search(/\&longitude=.+\&/) > 0){
+	// 		url = url.replace(/\&longitude=.+\&/, '&longitude=120.69094848632812&')
+	// 	}else if(url.search(/\&longitude=.+/) > 0){
+	// 		url = url.replace(/\&longitude=.+/, '&longitude=120.69094848632812')
+	// 	}else{
+	// 		console.warn("特殊定位修改：longitude修改失败");
+	// 	}
+	// 	
+	// 	if(url.search(/\&latitude=.+\&/) > 0){
+	// 		url = url.replace(/\&latitude=.+\&/, '&latitude=30.526451110839844&')
+	// 	}else if(url.search(/\&latitude=.+/) > 0){
+	// 		url = url.replace(/\&latitude=.+/, '&latitude=30.526451110839844')
+	// 	}else{
+	// 		console.warn("特殊定位修改：latitude修改失败");
+	// 	}
+	// 	
+	// 	console.log("特殊定位修改：成功--" + url)
+	// }
 	
 } catch (e) {
   console.warn("特殊定位修改：" + e);
