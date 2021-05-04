a1665515b129de00a3c398f23b622b7b494e82af
夏震 <xiazhen@xzdemacbook-pro.local>
2021-05-04 19:38:31 +0000

    debug

--- location/blue.js
+++ location/blue.js
@@ -9,16 +9,20 @@ try {
 		console.log("特殊定位修改：捕获成功");
 		
 		if(path.search(/\&longitude=\d+.\d+\&/) > 0){
+			console.log(111);
 			path = path.replace(/\&longitude=\d+.\d+\&/, '&longitude=120.69094848632812&')
 		}else if(path.search(/\&longitude=\d+.\d+\&/) > 0){
+			console.log(222);
 			path = path.replace(/\&longitude=\d+.\d+/, '&longitude=120.69094848632812')
 		}else{
 			console.warn("特殊定位修改：longitude修改失败");
 		}
 		
 		if(path.search(/\&latitude=\d+.\d\&/) > 0){
+			console.log(333);
 			path = path.replace(/\&latitude=\d+.\d+\&/, '&latitude=30.526451110839844&')
 		}else if(path.search(/\&latitude=.+/) > 0){
+			console.log(444);
 			path = path.replace(/\&latitude=\d+.\d+/, '&latitude=30.526451110839844')
 		}else{
 			console.warn("特殊定位修改：latitude修改失败");
