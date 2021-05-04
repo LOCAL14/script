4ba54ee065ebe7b298bb2dd6b00d5251a2eff089
夏震 <xiazhen@xzdemacbook-pro.local>
2021-05-04 20:10:45 +0000

    blue finish

--- location/blue.js
+++ location/blue.js
@@ -1,7 +1,7 @@
 //特殊定位修改 argo.blue.cn
 
-const desLot = ' + desLot + '
-const desLat = ' + desLat + '
+const desLot = '120.69094848632812'
+const desLat = '30.526451110839844'
 
 path = $request.path
 
@@ -14,7 +14,7 @@ try {
 		if(path.search(/\&longitude=\d+.\d+\&/) > 0){
 			path = path.replace(/\&longitude=\d+.\d+\&/, '&longitude=' + desLot + '&')
 		}else if(path.search(/\&longitude=\d+.\d+/) > 0){
-			path = path.replace(/\&longitude=\d+.\d+/, '&longitude=' + desLot + '')
+			path = path.replace(/\&longitude=\d+.\d+/, '&longitude=' + desLot)
 		}else{
 			console.log("特殊定位修改：longitude修改失败");
 		}
@@ -22,7 +22,7 @@ try {
 		if(path.search(/\&latitude=\d+.\d+\&/) > 0){
 			path = path.replace(/\&latitude=\d+.\d+\&/, '&latitude=' + desLat + '&')
 		}else if(path.search(/\&latitude=\d+.\d+/) > 0){
-			path = path.replace(/\&latitude=\d+.\d+/, '&latitude=' + desLat + '')
+			path = path.replace(/\&latitude=\d+.\d+/, '&latitude=' + desLat)
 		}else{
 			console.log("特殊定位修改：latitude修改失败");
 		}
@@ -36,7 +36,7 @@ try {
 		if(path.search(/\&lot=\d+.\d+\&/) > 0){
 			path = path.replace(/\&lot=\d+.\d+\&/, '&lot=' + desLot + '&')
 		}else if(path.search(/\&lot=\d+.\d+/) > 0){
-			path = path.replace(/\&lot=\d+.\d+/, '&lot=' + desLot + '')
+			path = path.replace(/\&lot=\d+.\d+/, '&lot=' + desLot)
 		}else{
 			console.log("特殊定位修改：lot修改失败");
 		}
@@ -44,9 +44,9 @@ try {
 		if(path.search(/\&lat=\d+.\d+\&/) > 0){
 			path = path.replace(/\&lat=\d+.\d+\&/, '&lat=' + desLat + '&')
 		}else if(path.search(/\&lat=\d+.\d+/) > 0){
-			path = path.replace(/\&lat=\d+.\d+/, '&lat=' + desLat + '')
+			path = path.replace(/\&lat=\d+.\d+/, '&lat=' + desLat)
 		}else if(path.search(/lat=\d+.\d+/) > 0){
-			path = path.replace(/lat=\d+.\d+/, 'lat=' + desLat + '')
+			path = path.replace(/lat=\d+.\d+/, 'lat=' + desLat)
 		}else{
 			console.log("特殊定位修改：lat修改失败");
 		}
